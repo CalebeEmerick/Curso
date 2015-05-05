bceac09c99160a8d23d145c5e50633380640c299
kalebexD <calebe_emerick@hotmail.com>
2015-05-05 12:21:13 -0300

    Conectando a banco, criando o controller de Produtos para teste.

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -10,7 +10,7 @@ namespace Quiron.LojaVirtual.Dominio.Entidade
     {
         public int ProdutoId { get; set; }
         public string Nome { get; set; }
-        public int Descricao { get; set; }
+        public string Descricao { get; set; }
         public decimal Preco { get; set; }
         public string Categoria { get; set; }
     }
